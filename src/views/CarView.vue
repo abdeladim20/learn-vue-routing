f5fde2288d894b4bebdf916611e6a93885a5a4a0
abdeladim20 <linuxacount1@gmail.com>
2025-05-06 10:21:29 +0100

    learning routes complete

--- src/views/CarView.vue
+++ src/views/CarView.vue
@@ -1,18 +1,34 @@
 <script setup>
-import { useRoute } from "vue-router"
+import { useRoute, useRouter, RouterView } from "vue-router"
 import cars from "../data/cars.json"
 
 const route = useRoute()
+const router = useRouter()
+const carId = parseInt(route.params.id)
+
+const showContact = () => {
+    if (carId === 4) {
+        return;
+    } else {
+        return router.push(`/cars/${carId}/contact`)
+    }
+}
+
 const car = cars.find(c => c.id === parseInt(route.params.id))
 
 
 </script>
 
 <template>
-    <div>
+    <div v-if="car">
         <h1>Car View</h1>
         <p>{{ car.name }}</p>
         <p>{{ car.year }}</p>
         <p>{{ car.price }}</p>
+        <button @click="showContact()">Click Me For Contact</button>
+        <RouterView />
+    </div>
+    <div v-else>
+        <h1>Car Not Found</h1>
     </div>
 </template>
